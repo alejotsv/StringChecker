c2e13c57bcf36ebaad3512bdd50e4552916e354e
Alejandro Salas <asalas.wd@outlook.com>
2022-10-09 16:46:45 -0400

    Add different String methods

--- src/com/java21days/StringChecker.java
+++ src/com/java21days/StringChecker.java
@@ -2,6 +2,12 @@ package com.java21days;
 
 public class StringChecker {
     public static void main(String[] args) {
-        System.out.println("This is the String Checker");
+        String str = "A Lannister always pays his debts.";
+        System.out.println("The string is " + "\"" + str + "\".");
+        System.out.println("The length of the string is " + str.length() + ".");
+        System.out.println("The character at position 6 is " + "\"" + str.charAt(6) + "\".");
+        System.out.println("The substring between characters 12 and 16 is " + "\"" + str.substring(12,16) + "\".");
+        System.out.println("The index of the first 't' is " + str.indexOf("t") + ".");
+        System.out.println("The index of the beginning of the substring \"debts\" is " + str.indexOf("debts") + ".");
     }
 }
